749a60041962177a4f8d13ac37f83cbab90c4fea
Arunava Nag <arunavaknag@berkeley.edu>
2026-01-17 03:58:58 -0800

    chore(40-01): export GainLossBarChart from index
    - Add barrel export for GainLossBarChart component
    - TypeScript verification passes

--- bullion-tracker-mobile/src/components/charts/index.ts
+++ bullion-tracker-mobile/src/components/charts/index.ts
@@ -2,3 +2,4 @@ export { ChartContainer } from './ChartContainer';
 export { TestChart } from './TestChart';
 export { PortfolioLineChart } from './PortfolioLineChart';
 export { AllocationPieChart } from './AllocationPieChart';
+export { GainLossBarChart } from './GainLossBarChart';
